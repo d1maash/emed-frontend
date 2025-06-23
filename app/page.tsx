c140ed79ed61f0cd2a3c4c3cf2a0c27068bfbf5a
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2025-06-23 17:30:36 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-black">
+      <p className="text-red-500 ">Salam developers</p>
+    </div>
+  );
+}
