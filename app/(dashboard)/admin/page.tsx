4b05d5033d6120d9c542cc03aabb0e3814bebe74
zhorik06bondarenko <zhorik06bondarenko@gmail.com>
2025-06-30 17:47:56 +0500

    feat: added top cards for stats in main admin page

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import AdminInfoCard from "./_components/AdminInfoCard";
+
+const adminCardTemplateInfo = [
+  {
+    title: "Число пользователей",
+    text: "11.8M",
+    change: +2.5,
+  },
+  {
+    title: "Врачи",
+    text: "11.8M",
+    change: +2.5,
+  },
+  {
+    title: "Призывники",
+    text: "11.8M",
+    change: -2.5,
+  },
+  {
+    title: "Координаторы",
+    text: "11.8M",
+    change: +2.5,
+  },
+  {
+    title: "Комиссия",
+    text: "11.8M",
+    change: -5.5,
+  },
+];
 
 const AdminPanelPage = () => {
-  return <div>AdminPanelPage</div>;
+  const adminCards = adminCardTemplateInfo;
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-row w-full overflow-y-auto gap-6">
+        {adminCards.map((card) => (
+          <AdminInfoCard key={card.title} {...card} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AdminPanelPage;
