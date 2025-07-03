3162e480caaeb8362fc9b7a5d8380bba4c4c36fb
zhorik06bondarenko <zhorik06bondarenko@gmail.com>
2025-07-04 00:44:31 +0500

    feat: added reusable component NavTabs with adaptive

--- app/(dashboard)/admin/system/page.tsx
+++ app/(dashboard)/admin/system/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+import { NavTab, NavTabs } from "@/components/NavTabs";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const pages = [
+  "Справочные данные",
+  "Рабочие часы",
+  "Правила логирования",
+  "Конфигурация интеграций",
+  "Параметры безопасности",
+];
+
+type Page = (typeof pages)[number];
 
 const AdminSystemPage = () => {
-  return <div>AdminSystemPage</div>;
+  const [page, setPage] = useState<Page>("Справочные данные");
+
+  return (
+    <div className="w-full flex flex-col gap-6 @container">
+      <h1 className="text-3xl font-medium">{page}</h1>
+      <NavTabs page={page}>
+        {pages.map((p) => (
+          <NavTab
+            key={p}
+            page={p}
+            active={page === p}
+            onClick={() => setPage(p)}
+          />
+        ))}
+      </NavTabs>
+    </div>
+  );
 };
 
 export default AdminSystemPage;
